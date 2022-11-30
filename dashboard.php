c3f7a6f74138852cfefa12b1802a68f9fc53d29e
KiflanSyawal <97747587+KiflanSyawal@users.noreply.github.com>
2022-11-30 16:49:03 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>pckomp</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+            <h1><a href="dashboard.php">pckomp</a></h1>
+            <ul>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="data_kategori.php">Data Kategori</a></li>
+                <li><a href="data_produk.php">Data Produk</a></li>
+                <li><a href="keluar.php">Log out</a></li>
+            </ul>
+            </div>
+        </header>
+        
+        <div class="section">
+            <div class="container">
+                <h3>Dashboard</h3>
+                <div class="box">
+                    <h4>Selamat Datang</h4>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>   
